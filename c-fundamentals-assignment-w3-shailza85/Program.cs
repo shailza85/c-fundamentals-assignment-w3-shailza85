2e518f2d9c44a1d1bcca139b87654685baf05c66
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-09-01 10:25:59 -0600

    Add project files.

---
+++ c-fundamentals-assignment-w3-shailza85/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_fundamentals_assignment_w3_shailza85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
